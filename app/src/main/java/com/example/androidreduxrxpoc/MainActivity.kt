63171bb7721493c314cf36cf9152192332575c99
Corey Lanier <iam@coreylanier.com>
2020-10-05 18:22:00 -0400

    able to add todos, not rerendering though

--- app/src/main/java/com/example/androidreduxrxpoc/MainActivity.kt
+++ app/src/main/java/com/example/androidreduxrxpoc/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.example.androidreduxrxpoc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidreduxrxpoc.actions.AddTodo
 import com.example.androidreduxrxpoc.adapter.ItemAdapter
+
 import com.example.androidreduxrxpoc.data.Datasource
+import com.example.androidreduxrxpoc.stores.store
+import org.reduxkotlin.StoreSubscription
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var storeSubscription: StoreSubscription
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myDataset = Datasource().loadTodos()
+        storeSubscription = store.subscribe { Datasource().loadTodos() }
+        var myDataset = Datasource().loadTodos()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
         recyclerView.setHasFixedSize(true)
+        val button: Button = findViewById(R.id.btn_add)
+        button.setOnClickListener {
+            addTodo()
+        }
+    }
+    private fun addTodo() {
+        val textBox: EditText = findViewById(R.id.text_add_todo)
+        val text = textBox.text.toString()
+        store.dispatch(AddTodo(text))
+        textBox.text.clear()
     }
 }
 
-fun addTodo() {
-
-}
